240ffa6aee79496b92ea0be3c3a3faeba98762b8
Arnab Mukherjee <mukherjeearnab988@gmail.com>
2025-12-31 03:08:51 +0530

    Update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = "https://restaurant-atmosphere.vercel.app";
+  const baseUrl = "https://asfitnesszone.vercel.app";
 
   return [
     {
